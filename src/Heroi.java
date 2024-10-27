d61ed1fcfc7f6a45a2fbae3eaf62c1bb8b0ef9cf
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-10-27 14:08:29 -0300

    Cria os atributos da classe Heroi

--- src/Heroi.java
+++ src/Heroi.java
@@ -1,6 +1,15 @@
 public class Heroi {
 
     private String nomeHeroi;
-    private Estatisticas estatisticas
+    private EstatisticasHeroi estatisticasHeroi;
+    private NivelHeroi nivelHeroi;
+    
+    public Heroi(String nomeHeroi, EstatisticasHeroi estatisticasHeroi, NivelHeroi nivelHeroi) {
+        this.nomeHeroi = nomeHeroi;
+        this.estatisticasHeroi = estatisticasHeroi;
+        this.nivelHeroi = nivelHeroi;
+    }
+
+    
 
 }
