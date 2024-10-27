9333416f3bfaae840f54c6da8d2ba7991faa1f75
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-10-27 14:25:45 -0300

    Criando os atributos e métodos iniciais e conclui o código da classe EstatisticasHeroi

--- src/EstatisticasHeroi.java
+++ src/EstatisticasHeroi.java
@@ -1,3 +1,28 @@
 public class EstatisticasHeroi {
 
+    private int numeroVitoria;
+    private int numeroDerrota;
+    
+    public EstatisticasHeroi(int numeroVitoria, int numeroDerrota) {
+        this.numeroVitoria = numeroVitoria;
+        this.numeroDerrota = numeroDerrota;
+    }
+
+    public int getNumeroVitoria() {
+        return numeroVitoria;
+    }
+
+    public void setNumeroVitoria(int numeroVitoria) {
+        this.numeroVitoria = numeroVitoria;
+    }
+
+    public int getNumeroDerrota() {
+        return numeroDerrota;
+    }
+
+    public void setNumeroDerrota(int numeroDerrota) {
+        this.numeroDerrota = numeroDerrota;
+    }
+
+    
 }
